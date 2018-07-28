49ae89a24dbfdab0d9dab169556af6ee40b59128
Gagan Suneja <gagan91suneja@gmail.com>
2018-07-28 20:04:09 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HttpClientModule  } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { GeneralService } from'./common/general.service';
+import { FormsModule } from '@angular/forms';
+import { HighlightDirective } from './highlight.directive';
+import { NewhighlightDirective } from './newhighlight.directive';
+import { CurrencyComponent } from './currency/currency.component';
+import   {  
+  MatButtonModule,
+  MatRadioModule,
+  MatSnackBarModule,
+  MatTableModule
+  
+  }from '@angular/material';
+  import {priceFormatPipe} from './common/pipes/priceFormat.pipe'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    HomepageComponent,
+    NewhighlightDirective,
+    HighlightDirective,
+    CurrencyComponent,
+    priceFormatPipe
+  ],    
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatSnackBarModule,
+    MatTableModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [GeneralService],
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
